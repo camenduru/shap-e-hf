92b34c32983977b0b427721397bb3778b79beb83
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-06 03:54:09 +0300

    test

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ from model import Model
 
 DESCRIPTION = '# [Shap-E](https://github.com/openai/shap-e)'
 
-if (SPACE_ID := os.getenv('SPACE_ID')) is not None:
-    DESCRIPTION += f'\n<p>For faster inference without waiting in queue, you may duplicate the space and upgrade to GPU in settings. <a href="https://huggingface.co/spaces/{SPACE_ID}?duplicate=true"><img style="display: inline; margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate Space" /></a></p>'
-if not torch.cuda.is_available():
-    DESCRIPTION += '\n<p>Running on CPU 🥶 This demo does not work on CPU.</p>'
-
 model = Model()
 
 with gr.Blocks(css='style.css') as demo:
